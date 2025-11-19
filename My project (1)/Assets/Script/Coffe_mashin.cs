9e99f5929462c974efff01b0e73ab0942613b176
prosperousrum-cpu <prosperous.rum@gmail.com>
2025-11-19 23:01:14 +0700

    code_update

---
+++ My project (1)/Assets/Script/Coffe_mashin.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Coffe_machine : MonoBehaviour
+{
+    [SerializeField] private float _tocenPerCup = 0.25f;
+
+
+
+    void Start()
+    {
+
+
+
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
